7665cce2b21530656a7f4b6b634fcd4603ba492b
92Phyton <cuellar.2023jorge@gmail.com>
2026-08-19 23:24:42 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function mostrarContenido(id) {
-    const contenido= document.getElementById(id);
-    if (contenido.style.display==="block"){
-        contenido.style.display="none";
-    }else{
-        contenido.style.display="block";
+
+    const contenido = document.getElementById(id);
+
+    if (contenido.style.display === "block") {
+        contenido.style.display = "none";
+    } else {
+
+        // Ocultar los demás contenidos
+        document.querySelectorAll(".contenido").forEach(function(elemento) {
+            elemento.style.display = "none";
+        });
+
+        // Mostrar el seleccionado
+        contenido.style.display = "block";
     }
-        
 }
